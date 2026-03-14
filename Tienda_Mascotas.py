91872eeaa8f990cb0cf4fd83c94af64cfa6ada88
Diego-capu <diegoospino487@gmail.com>
2026-03-14 15:43:49 -0500

    Create Tienda_Mascotas.py

---
+++ Tienda_Mascotas.py
@@ -0,0 +1,14 @@
+print("Tienda de mascotas")
+print("Pida su mascota")
+print("Perro")
+print("Gato")
+print("Conejo")
+
+mascotas = input("-")
+
+if mascotas == "perro" or "Conejo" or "CONEJO":
+    print("Sacalo a pasear en las noches")
+elif mascotas == "gato" or "Conejo" or "CONEJO":
+    print("Dale pescado")
+elif mascotas == "conejo" or "Conejo" or "CONEJO":
+    print("Dale lechuga")
