4c2e5d134f40f1938c15bec01ab37fa6c5a1cea9
Diego-capu <diegoospino487@gmail.com>
2026-03-14 15:42:25 -0500

    Update Peluqueria.py

--- Peluqueria.py
+++ Peluqueria.py
@@ -1,1 +1,11 @@
-
+print("Hora de llegada")
+while True:
+    hora = int(input("-"))
+    if 6 <= hora and hora < 11:
+        print("Horario de llegada (Mañana)")
+    elif 12 <= hora and hora < 17:
+        print("Horario de llegada (Tarde)")
+    elif 18 <= hora and hora < 22:
+        print("Horario de llegada (Noche)")
+    else:
+        print("Fuera de horario")
