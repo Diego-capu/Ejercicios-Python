a596d9c41fef667017bd494120d23187695d2e07
Diego-capu <diegoospino487@gmail.com>
2026-03-14 15:40:49 -0500

    Create Cine.py

---
+++ Cine.py
@@ -0,0 +1,11 @@
+print("Ingrese su edad")
+edad = int(input("-"))
+
+if edad < 12:
+    print("Total a pagar 8000")
+elif edad >= 12:
+    print("Total a pagar 12000")
+elif edad > 60:
+    print("Total a pagar")
+else:
+    print("")
